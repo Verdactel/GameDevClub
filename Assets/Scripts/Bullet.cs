d1cfd6ca2bb27b2eb77be93466bea5023bc0634c
Verdactel <1kingdom4all@gmail.com>
2021-07-10 12:07:04 -0400

    Fixed bullet

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,9 +6,18 @@ public class Bullet : MonoBehaviour
 {
     public float m_speed = 20f;
     public float m_lifetime = 2f;
-    public Rigidbody rb;
-    void Start()
+
+    private void Update()
     {
-        rb.velocity = transform.forward * m_speed * Time.deltaTime;
+        this.gameObject.transform.position += new Vector3(m_speed * Time.deltaTime, 0f, m_speed * Time.deltaTime);
+
+        if(m_lifetime >= 0.0f)
+        {
+            m_lifetime -= Time.deltaTime;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
