8b5e475266c676231f137a13c8ce92b7c87b0d3d
Verdactel <1kingdom4all@gmail.com>
2021-07-10 12:29:12 -0400

    Bullet stuff

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,12 +4,29 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public GameObject targetObj;
+    private Transform m_target;
+
     public float m_speed = 20f;
     public float m_lifetime = 2f;
 
+    private void Awake()
+    {
+        if (!targetObj) Debug.Log("No object selected");
+        else m_target = targetObj.transform;
+    }
     private void Update()
     {
-        this.gameObject.transform.position += new Vector3(m_speed * Time.deltaTime, 0f, m_speed * Time.deltaTime);
+        if (!targetObj) Debug.Log("No object selected");
+        //MoveTo
+        else 
+        {
+            Debug.Log("Moving towards " + m_target.position);
+            float step = m_speed * Time.deltaTime;
+            this.transform.position = Vector3.MoveTowards(transform.position, m_target.position, step);
+        }
+        
+        //this.gameObject.transform.position += new Vector3(m_speed * Time.deltaTime, 0f, m_speed * Time.deltaTime);
 
         if(m_lifetime >= 0.0f)
         {
